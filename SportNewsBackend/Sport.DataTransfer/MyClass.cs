ace9661830e6989c29afb9bcf16cbf8cfe1ecfe9
Oleh Monchuk <olegio.monya@gmail.com>
2022-10-26 18:03:18 +0200

    Update class MyClass in branch developer

--- SportNewsBackend/Sport.DataTransfer/MyClass.cs
+++ SportNewsBackend/Sport.DataTransfer/MyClass.cs
@@ -2,10 +2,15 @@
 {
     public class MyClass
     {
-        public MyClass() 
+        private int id;
+        private string name;
+        public MyClass(int id, string name) 
         {
+            this.id = id;
+            this.name = name;
             ToString();
         }
+       
         public override string ToString()
         {
             return "Hello this is SportNews";
